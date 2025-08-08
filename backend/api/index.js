4731b949d982b6376f4a8472efb3858d0e8c7543
anshul_c0des <anshuldhariya5@gmail.com>
2025-08-08 17:45:24 +0530

    serverless function crash

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,40 +1,39 @@
 const express = require('express');
-const cors = require('cors');
 const mongoose = require('mongoose');
-const messageRoutes = require('../routes/messageRoutes');
+const cors = require('cors');
+const messageRoutes = require('../routes/messageRoutes.js');
 
 const app = express();
-
 app.use(cors({
-origin: 'https://whatsapp-e13h5ej92-anshuls-projects-ad041669.vercel.app',
-credentials: false
-}));
-
+    origin: 'https://whatsapp-hqejckmqb-anshuls-projects-ad041669.vercel.app/',  
+    credentials: false,
+  }));    
 app.use(express.json());
 
-app.options('*', cors());
-
 let isConnected = false;
+
 async function connectDB() {
-if (isConnected) return;
-await mongoose.connect(process.env.MONGO_URI, {
+  if (isConnected) return;
+  await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
-isConnected = true;
-console.log('MongoDB connected');
+  });
+  isConnected = true;
+  console.log('MongoDB connected');
 }
 
-await connectDB();
-
 app.get('/', (req, res) => {
-res.send('Backend is up and running');
+    res.send('Backend is up and running');
 });
 
-app.get('/api/ping', (req, res) => {
-res.json({ message: 'pong' });
+app.get('/api/ping', async (req, res) => {
+  await connectDB();
+  res.json({ message: 'pong' });
 });
 
-app.use('/api', messageRoutes);
+app.use('/', async (req, res, next) => {
+  await connectDB();
+  messageRoutes(req, res, next);  
+});
 
 module.exports = app;
