57b15e42b2d10f89723c23cefd2fe8ee2029d70f
anshul_c0des <anshuldhariya5@gmail.com>
2025-08-08 18:07:29 +0530

    config vercel

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const messageRoutes = require('../routes/messageRoutes.js');
+const messageRoutes = require('../routes/messageRoutes');
 
 const app = express();
 
