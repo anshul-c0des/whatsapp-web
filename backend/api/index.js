2977a3402b4e6e729d56b39a45aa320659eaebb2
anshul_c0des <anshuldhariya5@gmail.com>
2025-08-08 17:27:04 +0530

    modified vercel.json

--- backend/api/index.js
+++ backend/api/index.js
@@ -6,31 +6,31 @@ const messageRoutes = require('../routes/messageRoutes');
 const app = express();
 
 app.use(cors({
-  origin: '*',
-  credentials: false
+origin: 'https://whatsapp-e13h5ej92-anshuls-projects-ad041669.vercel.app/',
+credentials: false
 }));
 
 app.use(express.json());
 
 let isConnected = false;
 async function connectDB() {
-  if (isConnected) return;
-  await mongoose.connect(process.env.MONGO_URI, {
+if (isConnected) return;
+await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  });
-  isConnected = true;
-  console.log('MongoDB connected');
+});
+isConnected = true;
+console.log('MongoDB connected');
 }
 
 await connectDB();
 
 app.get('/', (req, res) => {
-  res.send('Backend is up and running');
+res.send('Backend is up and running');
 });
 
 app.get('/api/ping', (req, res) => {
-  res.json({ message: 'pong' });
+res.json({ message: 'pong' });
 });
 
 app.use('/api', messageRoutes);
