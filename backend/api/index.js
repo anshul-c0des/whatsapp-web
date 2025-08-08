c2158d90bf384d9ce3f526c133fcdb0252e4ae21
anshul_c0des <anshuldhariya5@gmail.com>
2025-08-08 18:45:29 +0530

    url

--- backend/api/index.js
+++ backend/api/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 // ✅ CORS configuration
 app.use(cors({
-  origin: 'https://whatsapp-lopf2hhdc-anshuls-projects-ad041669.vercel.app',
+  origin: 'https://whatsapp-web-tau.vercel.app',
   credentials: true,
 }));
 
